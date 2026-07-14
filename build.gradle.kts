4982a8198e3e3d793c2128add31f9bdfa8ea0416
dlajfn12-commits <dlajfn12@gmail.com>
2026-07-14 20:44:01 +0900

    Step 1: Project setup - Build configuration and gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,7 @@ plugins {
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+task("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
